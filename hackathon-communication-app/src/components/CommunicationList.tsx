c065c48b68d47e7118578990cbfc0d5a42d6b221
Nicole Menezes <nclmenezess@gmail.com>
2026-01-26 00:01:29 +0000

    alteração da variant do titulo da pagina

--- hackathon-communication-app/src/components/CommunicationList.tsx
+++ hackathon-communication-app/src/components/CommunicationList.tsx
@@ -150,7 +150,7 @@ const CommunicationList: React.FC = () => {
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
       <Box sx={{ mb: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom fontSize="2rem" sx={{ fontFamily: 'Arial, sans-serif'}}>
+        <Typography variant="h6" component="h1" gutterBottom fontSize="2rem" sx={{ fontFamily: 'Arial, sans-serif'}}>
          Caixa de Mensagens Next Coders
         </Typography>
       </Box>
