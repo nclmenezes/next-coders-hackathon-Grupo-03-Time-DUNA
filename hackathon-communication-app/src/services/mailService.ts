114fa7cc076a0600c29b8f5de04b2a541d2bbc44
Nicole Menezes <nclmenezess@gmail.com>
2026-01-28 22:22:02 -0300

    alterações

--- hackathon-communication-app/src/services/mailService.ts
+++ hackathon-communication-app/src/services/mailService.ts
@@ -6,25 +6,25 @@ class MailService {
 
   // Get all emails (simulated API call)
   async getMails(): Promise<Email[]> {
-    const response=await api.get<Email[]>('/api/Email');
+    const response=await api.get<Email[]>('api/Email');
     return response.data;
   }
 
   // POST - Criar [HttpPost("create")]
   async addEmail(data: { name: string; email: string; class: string; role: string }) {
-    const response = await api.post('/api/Email/create', data);
+    const response = await api.post('api/Email/create', data);
     return response.data;
   }
 
   // PUT - Atualizar [HttpPut("{id}")]
   async updateEmail(id: string, data: Partial<Email>) {
-    const response = await api.put(`/api/Email/${id}`, data);
+    const response = await api.put(`api/Email/${id}`, data);
     return response.data;
   }
 
   // DELETE - Remover [HttpDelete("{id}")]
   async deleteEmail(id: string) {
-    const response = await api.delete(`/api/Email/${id}`);
+    const response = await api.delete(`api/Email/${id}`);
     return response.data;
   }
   
@@ -36,20 +36,20 @@ class MailService {
     body: payload.message
   };
 
-  const response = await api.post('/api/Email', emailRequest);
+  const response = await api.post('api/Email', emailRequest);
   return response.status;
 }
 
 
   // rota de busca
   async searchEmails(term: string): Promise<Email[]> {
-    const response=await api.get<Email[]>(`/api/Email/search?term=${term}`);
+    const response=await api.get<Email[]>(`api/Email/search?term=${term}`);
     return response.data;
   }
 
   //rota de emails enviados
   async sentEmails(): Promise<any[]> {
-    const response=await api.get('/api/Email/sent-emails');
+    const response=await api.get('api/Email/sent-emails');
     return response.data;
   }
 }
